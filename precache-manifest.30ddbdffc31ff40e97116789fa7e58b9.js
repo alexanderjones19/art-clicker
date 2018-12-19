380aa67674d6df89334da839f2b4106401d82808
alexanderjones19 <alexanderjones4215@gmail.com>
2018-12-18 21:05:08 -0500

    Updates

---
+++ precache-manifest.30ddbdffc31ff40e97116789fa7e58b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aee3106cf424c8556969",
+    "url": "/art-clicker/static/js/runtime~main.aee3106c.js"
+  },
+  {
+    "revision": "bba87d522e59ee990ac8",
+    "url": "/art-clicker/static/js/main.bba87d52.chunk.js"
+  },
+  {
+    "revision": "724fb07e0ed781108fd3",
+    "url": "/art-clicker/static/js/1.724fb07e.chunk.js"
+  },
+  {
+    "revision": "bba87d522e59ee990ac8",
+    "url": "/art-clicker/static/css/main.b41b7519.chunk.css"
+  },
+  {
+    "revision": "4ffceae4e4ba24949d49e85a17a05ab9",
+    "url": "/art-clicker/index.html"
+  }
+];
