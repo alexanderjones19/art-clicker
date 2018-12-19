77bb1f487107bd20277cc086e5da230ceb4cbc44
alexanderjones19 <alexanderjones4215@gmail.com>
2018-12-19 04:41:54 -0500

    updated stylings

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ const Footer = () => {
   return (
     <footer className="page-footer orange accent-4">
       <div className="footer-copyright orange darken-4">
-        <div className="container">
-          © Alexander Jones
+        <div className="container center-align">
+          &copy; Alexander Jones
         </div>
       </div>
     </footer>
--- src/components/Header.js
+++ src/components/Header.js
@@ -7,11 +7,12 @@ const Header = () => {
         <div className="section">
           <div className="row">
             <div className="col s12">
-              <h1 className="center-align">Art Clicker</h1>
-              <h4 className="center-align flow-text">Click these paintings to score. Beware though, don't click the same one twice.</h4>
+              <h1 id="header-title" className="center-align">Art Clicker</h1>
+              <h4 id="header-subtext" className="center-align flow-text">Click these paintings to score. Beware though, don't click the same one twice.</h4>
             </div>
           </div>
         </div>
+        <div className="divider"></div>
       </div>
     </header>
   );
